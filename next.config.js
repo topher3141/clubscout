8690fa00b4aac2f311a5ee8e8be02085bdbbd4d7
topher3141 <TOPHER3141@GMAIL.COM>
2026-01-25 14:39:45 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com"
+      }
+    ]
+  }
 };
+
 module.exports = nextConfig;
