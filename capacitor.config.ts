10b2e56d1ccf81d6a8390a8fc8732c86e3ca978b
topher3141 <TOPHER3141@GMAIL.COM>
2026-02-16 13:19:34 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,9 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.lcliquidations.clubscout",   // change if you want
-  appName: "ClubScout",                   // change if you want
-  webDir: "public",                       // MUST exist in repo
-
+  appId: "com.lcliquidations.clubscout",
+  appName: "ClubScout",
+  webDir: "public"
 };
 
 export default config;
