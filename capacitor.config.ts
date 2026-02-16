5c6b7e458d2fd76cceee6fe984a4429c928bae03
topher3141 <TOPHER3141@GMAIL.COM>
2026-02-16 11:35:07 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.lcliquidations.clubscout',     // e.g. com.lcliquidations.lpnfinder
-  appName: 'clubSCOUT',                  // e.g. LPNFinder
-  webDir: 'public',                          // not used in Option B, but required
+  appId: "com.lcliquidations.clubscout",   // change if you want
+  appName: "ClubScout",                   // change if you want
+  webDir: "public",                       // MUST exist in repo
   server: {
-    url: 'https://https://clubscout-lilac.vercel.app/',           // e.g. https://lpnfinder.vercel.app
-    androidScheme: 'https'
-    // DO NOT set cleartext=true for production HTTPS
+    url: "https://clubscout-lilac.vercel.app", // <-- your Vercel URL
+    androidScheme: "https"
   }
 };
 
