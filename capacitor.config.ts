302d31dca44a794f220f65a6715e21f5afdddb67
topher3141 <TOPHER3141@GMAIL.COM>
2026-02-16 14:00:46 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.lcliquidations.clubscout",
   appName: "ClubScout",
-  webDir: "public"
+  webDir: "public", // must exist
+  server: {
+    url: "https://clubscout-lilac.vercel.app/",
+    androidScheme: "https"
+  }
 };
 
 export default config;
