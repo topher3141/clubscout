47554be87358b46c2d818eb84786d72b0cb39c52
topher3141 <TOPHER3141@GMAIL.COM>
2026-02-16 12:03:58 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,7 @@ const config: CapacitorConfig = {
   appId: "com.lcliquidations.clubscout",   // change if you want
   appName: "ClubScout",                   // change if you want
   webDir: "public",                       // MUST exist in repo
-  server: {
-    url: "https://clubscout-lilac.vercel.app", // <-- your Vercel URL
-    androidScheme: "https"
-  }
+
 };
 
 export default config;
